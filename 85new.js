2f6c706f65f7e7abb603e502727cc9f985a4a131
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-01 16:34:01 +0200

    triAngleArea with two option in js

---
+++ 85new.js
@@ -0,0 +1,7 @@
+var a = prompt ("wpisz wartość podstawy, np. 2"),
+    h = prompt ("wpisz wartość wysokości, np. 4"),
+    triangleArea = a*h/2;
+alert("pole trójkąta to " + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+  
